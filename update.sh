b0f8978b4b1115739453cc1a961960815b18db36
Brian Han <han12@llnl.gov>
2022-04-15 15:34:20 -0700

    Take off dog cat training wheels

--- update.sh
+++ update.sh
@@ -4,17 +4,7 @@ set -v
 
 exit_code=0
 
-# Clear output files
-git rm -r --cache cat*/
-git rm -r --cache dog*/
-rm -rf cat*/
-rm -rf dog*/
-
-# Copy over new output files
-cp -R ../cat .
-cp -R ../dog .
-
-# Update README
+# Update README header
 today_date="$(date +"%Y-%m-%d %H%M")"
 curr_date="$(date +"%m-%d-%y_%H%M")"
 commit_hash=$(cd .. && git log -1 --format="%h")
@@ -25,13 +15,27 @@ echo \#\#\# Git Commit Hash: ${commit_hash} >> README.md
 echo "System | Configuration Status | Build Status | Unit Test Status | Integrated Test Status" >> README.md
 echo "--- | --- | --- | --- | ---" >> README.md
 
-# Determine exit codes and update README entry for system
-for system in dog cat; do
+# Determine exit codes and update README entry for each system
+for system in quartz lassen; do
   
-  config_code=1
+  # Remove previous output files
+  git rm -r --cache ${system}*/
+  rm -rf ${system}*/
+
+  # Copy over new output files
+  cp -R ../${system} .
+
+  # Remove any large files
+  (cd ${system} && find . -size +25M -delete)
+
+  # Add files to git
+  git add ${system}
+
+  # Determine if each step succeeded or failed
+  if find . -name "*${system}*" -a -name "*configure*" -a -name "*SUCCESS*" | grep .; then config_code=0; else config_code=1; fi
   if find . -name "*${system}*" -a -name "*build*" -a -name "*SUCCESS*" | grep .; then build_code=0; else build_code=1; fi
-  unit_test_code=1
-  integrated_test_code=1
+  if find . -name "*${system}*" -a -name "*unit_test*" -a -name "*SUCCESS*" | grep .; then unit_test_code=0; else unit_test_code=1; fi
+  if find . -name "*${system}*" -a -name "*integrated_test*" -a -name "*SUCCESS*" | grep .; then integrated_test_code=0; else integrated_test_code=1; fi
 
   (( exit_code |= config_code | build_code | unit_test_code | integrated_test_code ))
 
@@ -45,29 +49,27 @@ for system in dog cat; do
   unit_test_log=${system}_unit_test
   integrated_test_log=${system}_integrated_test
 
-  # Updates/Displays last successful commit on success/failure
+  # Displays success/failure for each step, with last successful commit on failure
   declare -a codes=( $config_code $build_code $unit_test_code $integrated_test_code )
   declare -a statuses=( ${config_log} ${build_log} ${unit_test_log} ${integrated_test_log} )
   declare -a status_messages=( "" "" "" "" )
 
-  # for ((i=0;i<4;++i)); do
-  #     last_commit_cmd="awk '/${statuses[i]}/ {print \$2}' last_passed.txt"
-  #     last_commit=$(eval $last_commit_cmd)
+  for ((i=0;i<4;++i)); do
+      last_commit_cmd="awk '/${statuses[i]}/ {print \$2}' last_passed.txt"
+      last_commit=$(eval $last_commit_cmd)
 
-  #     if [ ${codes[i]} -eq 0 ] ; then
-  #       sed -i "/^ *${statuses[i]} / s/${last_commit}/${commit_hash}/" last_passed.txt
-  #     else
-  #       status_messages[i]="($last_commit last passing)" 
-  #     fi
-  # done
+      if [ ${codes[i]} -eq 0 ] ; then
+        sed -i "/^ *${statuses[i]} / s/${last_commit}/${commit_hash}/" last_passed.txt
+      else
+        status_messages[i]="($last_commit last passing)" 
+      fi
+  done
 
   echo "$system | $config_status ${status_messages[0]} | $build_status ${status_messages[1]} | $unit_test_status ${status_messages[2]} | $integrated_test_status ${status_messages[3]}" >> README.md
 
 done
 
-# Push/Amend changes to github
-git add cat
-git add dog
+# Push/Amend rest of changes to github
 git add README.md
 git add last_passed.txt
 git commit -m "Date: ${curr_date} ; Git Commit: ${commit_hash}"
